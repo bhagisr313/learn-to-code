d00a25a01ecf7090bf91fcd96f0087f69727b134
Bharghavi <bhagisr313@gmail.com>
2026-04-09 15:11:48 -0400

    add more programs involving condition-basics

--- first-100/theme-1-basic-printing-patterns.py
+++ first-100/theme-1-basic-printing-patterns.py
@@ -1,5 +1,34 @@
 # Problem 1 : Print numbers from 1 to N
+if n <= 0:
+    print("Invalid input")
+else:
+    for i in range(1, n + 1):
+        print(i)
 # Problem 2 : Print even numbers up to N
+if n <= 0:
+    print("Invalid input")
+else:
+    for i in range(2, n + 1, 2):
+            print(i)
 # Problem 3 : Print numbers in reverse
+if n <= 0:
+    print("Invalid input") 
+else:
+    for i in range(n,0,-1):
+        print(i)  
 # Problem 4 : Print sum of first N numbers
+if n <= 0:
+    print("Invalid input")
+else:
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i
+    print(sum)
 # Problem 5 : Print factorial of N
+if n < 0:
+    print("Invalid input")
+else:
+    factorial = 1
+    for i in range(2, n + 1):
+        factorial *= i
+    print(factorial)
---
+++ first-100/theme-2-conditions-basics.py
@@ -0,0 +1,43 @@
+# Check if number is positive/negative/zero
+num = 5
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+# Check even or odd
+num = 4
+if num % 2 == 0:
+    print("Even")
+else:   
+    print("Odd")
+
+# Find greatest of two numbers
+num1 = 90
+num2 = 90
+if num1 > num2:
+    print(num1)
+elif num1 == num2:
+    print("Both are equal")
+else:
+    print(num2)
+
+# Find greatest of three numbers
+num1 = 105
+num2 = 105
+num3 = 15
+if num1 >= num2 and num1 >= num3:
+    print(num1)
+elif num2 >= num1 and num2 >= num3:
+    print(num2)
+else:
+    print(num3)
+
+# Check leap year
+year = 1900
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print("It's a leap year")
+else:
+    print("It's not a leap year")
